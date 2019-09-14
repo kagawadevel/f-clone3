d8a94a690b40d3ca1858e4eda09c4d37181e8e06
kakegawa <cmuwni@gmail.com>
2019-09-14 16:30:49 +0900

    画像投稿機能を追加

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -69,6 +69,6 @@ class PostingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def posting_params
-      params.require(:posting).permit(:title, :content, :image)
+      params.require(:posting).permit(:title, :content, :image, :iamge_cache)
     end
 end
--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,2 +1,3 @@
 class Posting < ApplicationRecord
+  mount_uploader :image, ImageUploader
 end
