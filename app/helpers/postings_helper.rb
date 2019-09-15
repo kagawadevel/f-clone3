fc897bac650fec4d41f648842cc477587ecc24db
kakegawa <cmuwni@gmail.com>
2019-09-15 10:25:15 +0900

    投稿内容確認画面を修正

--- app/helpers/postings_helper.rb
+++ app/helpers/postings_helper.rb
@@ -1,2 +1,9 @@
 module PostingsHelper
+  def choose_new_or_edit
+    if action_name == "new" || action_name == "confirm" || action_name == "create"
+      confirm_postings_path
+    elsif action_name == "edit"
+      posting_path
+    end
+  end
 end
